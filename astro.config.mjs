a33b0b71cb392f9f3fc0697f0f9082b2831e781d
Arpita Patnaik <patnaikarpita10@gmail.com>
2025-08-05 01:24:19 -0400

    config file changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 
 export default defineConfig({
-  site: "https://Arpita31.github.io",
-  base: "",
-  integrations: [
-    tailwind(),
-    react(),
-    sitemap(),
-    compress({
-      CSS: true,
-      HTML: {
-        removeAttributeQuotes: false,
-        removeComments: false,
-      },
-      Image: true,
-      JavaScript: true,
-      SVG: true,
-    }),
-  ],
-  output: "static",
-  build: {
-    assets: "assets",
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: "assets/[name].[hash][extname]",
-        },
-      },
-    },
-  },
+  base: "/portfolio",
+  site: "https://arpita31.github.io",
 });
